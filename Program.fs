00b4dd6bdbdf3285fc98000d470598322b6f2349
Sean Scally <scally@users.noreply.github.com>
2022-01-16 16:47:36 -0800

    Early days

---
+++ Program.fs
@@ -0,0 +1,47 @@
+﻿open System.IO
+open type System.Environment
+
+let puts (str: string) = printfn $"{str}"
+
+let logDir =
+  [| GetFolderPath(SpecialFolder.Personal)
+     "Library/Application Support/com.wizards.mtga/Logs/Logs" |]
+  |> Path.Combine
+
+let logFiles = Directory.GetFiles logDir
+let mostRecentLogFile = (logFiles |> Array.head)
+
+let parseLine (line: string) =
+  let parts = line.Split "==> "
+
+  if parts.Length = 1 then
+    let otherParts = line.Split "<== "
+
+    if otherParts.Length = 1 then
+      None
+    else
+      Some otherParts.[1]
+  else
+    Some parts.[1]
+
+let parseLineIntoEntry (line: string) =
+  //[id] [UnityCrossThreadLogger]==> MessageName JSONString
+  //[id] [UnityCrossThreadLogger]<== MessageName JSONString
+  let parts = parseLine line
+
+  if parts.IsNone then
+    None
+  else
+    let subParts = parts.Value.Split(" ")
+
+    Some
+      {| MessageName = subParts.[0]
+         Payload = subParts.[1] |}
+
+File.ReadLines mostRecentLogFile
+|> Seq.filter (fun line ->
+  line.Split(" ")
+  |> Array.exists (fun part -> part.IndexOf("UnityCrossThreadLogger") > -1))
+|> Seq.map parseLineIntoEntry
+|> Seq.filter (fun e -> e.IsSome)
+|> Seq.iter (fun e -> puts e.Value.Payload)
